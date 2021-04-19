73e3ccc89eab49747dc06029d6c7c7e2bdfb2b84
christophe <christophegueroult@gmail.com>
2021-04-19 14:28:36 +0200

    modules agiles et metiers and imports, exports de modules

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,12 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { NgModule } from '@angular/core';
+import { IconsModule } from '../icons/icons.module';
+import { LoginModule } from '../login/login.module';
+import { TemplatesModule } from '../templates/templates.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [IconsModule, LoginModule, TemplatesModule],
 })
-export class CoreModule { }
+export class CoreModule {}
