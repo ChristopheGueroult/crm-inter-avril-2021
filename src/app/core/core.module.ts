1c1b5c36452bc9b2881a64b40e3c7d1be969d2cb
christophe <christophegueroult@gmail.com>
2021-04-19 16:27:20 +0200

    ui component witdh multiple transclusions

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
 import { TemplatesModule } from '../templates/templates.module';
+import { UiModule } from '../ui/ui.module';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, NavComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [IconsModule, LoginModule, TemplatesModule],
+  exports: [
+    IconsModule,
+    LoginModule,
+    TemplatesModule,
+    UiModule,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+  ],
 })
 export class CoreModule {}
