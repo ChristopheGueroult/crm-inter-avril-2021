97709f4eaf5000627543c82e4d7a1a3c3ef68237
christophe <christophegueroult@gmail.com>
2021-04-20 10:34:52 +0200

    all pages components and routing modules

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { LoginRoutingModule } from './login-routing.module';
+import { PageForgotComponent } from './pages/page-forgot/page-forgot.component';
+import { PageResetComponent } from './pages/page-reset/page-reset.component';
+import { PageSignUpComponent } from './pages/page-sign-up/page-sign-up.component';
+import { PageSignInComponent } from './pages/page-sign-in/page-sign-in.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageSignInComponent,
+    PageSignUpComponent,
+    PageResetComponent,
+    PageForgotComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    LoginRoutingModule
   ]
 })
 export class LoginModule { }
