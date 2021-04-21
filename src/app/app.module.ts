f7c55566ca9a0631105cae926bf032d97ab4d74a
christophe <christophegueroult@gmail.com>
2021-04-21 12:13:39 +0200

    Pipes angular, LOCAL_ID and DEFAULT_CURRENCY_CODE

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,20 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import localeFr from '@angular/common/locales/fr';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
-import { HttpClientModule } from '@angular/common/http';
-
+// the second parameter 'fr' is optional
+registerLocaleData(localeFr, 'fr');
 @NgModule({
-  declarations: [
-    AppComponent,
+  declarations: [AppComponent],
+  imports: [BrowserModule, CoreModule, AppRoutingModule, HttpClientModule],
+  providers: [
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
   ],
-  imports: [
-    BrowserModule,
-    CoreModule,
-    AppRoutingModule,
-    HttpClientModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
